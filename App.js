e61382f6dd46eaddf848b6b576b62a0ff0a9c7c4
Divya Tiwari <me@divyatiwari.me>
2024-04-12 15:03:48 +0530

    React CDN stopped working; Installed react and react-dom

--- App.js
+++
@@ -1,3 +0,0 @@
-const heading = React.createElement('h1', {}, 'Hello world from React JS world!!');
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+/**
+ * createElement takes 3 arguments
+ * tagname, attributes object, children - contains value of tag or children
+ * heading will be a JS object and not a h1 tag; it will contain "props" field - combination of attributes and children
+ */
+const heading = React.createElement("h1", {id: "heading", test: "random-string"}, "Hello world from React JS world!!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+/**
+ * Let's create nested div using react
+ * <div class="parent">
+ *  <div class="child">
+ *      <h1>Hello World in nested div!</h1>
+ *  </div>
+ * </div>
+ */
+
+const parent = React.createElement("div", {id: "parent"}, 
+    React.createElement("div", {id: "child"}, 
+    React.createElement("h1", {}, "Hello world in nested div!")))
+
+// render method is responsible for creating h1 tag out of the heading object
+root.render(parent);
